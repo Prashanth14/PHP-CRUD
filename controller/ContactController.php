c2dbdac5ee430ad2a59172491d0043350efd4913
Prashanth14 <kadireprashanthreddy1@gmail.com>
2020-09-02 14:58:22 +0530

    initial commit

---
+++ controller/ContactController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $contacts= Conatct::all();
+        return view('contacts/index',compact('contacts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('contacts/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData=request()->validate([
+            'name'=>'required',
+            'lastname'=>'required',
+            'phonenum'=>'required'
+        ]);
+        Contact::create($validatedData);
+        return redirect('/contacts');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Contact $contact)
+    {
+        return view('contacts/show',compact('contact'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Contact $contact)
+    {
+        $validatedData=request()->validate([
+            'name'=>'required',
+            'lastname'=>'required',
+            'phonenum'=>'required'
+        ]);
+
+        $contact->update($validatedData);
+        return redirect('/contacts');
+    }
+    
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Contact $contact)
+    {
+    $contact->delete();
+    return redirect('/contacts');
+    }
+}
